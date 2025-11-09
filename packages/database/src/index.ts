cfa1eb4d393b0ff303686e56d7bd6ce214a2bafa
Bote (Porter) Wang <wang.bot@northeastern.edu>
2025-11-09 00:21:53 -0500

    change

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -4,5 +4,5 @@ export const db = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['warn', 'error'],
 })
 
-export * from './generated/index.js'
+export * from './generated/index.js' 
 export type { User, Room, RoomMember, Session } from './generated/index.js'
